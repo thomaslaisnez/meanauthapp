093ce12f7779dd8a04c9e205806a0fd66db258b5
thomaslaisnez <thomaslaisnez@live.be>
2017-08-14 00:24:07 +0200

    part 2 of tutorial done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User =  require('../models/user');
+
 // Register
-router.get('/register', function(req, res, next){
-    res.send('Register');
+router.post('/register', function(req, res, next){
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, function(err, user){
+        if(err){
+            res.json({success: false, msg:'Failed to register user'});
+        } else {
+            res.json({success: true, msg:'User is now registered'});
+        }
+    });
 });
 
 // Authenticate
@@ -14,11 +32,6 @@ router.post('/authenticate', function(req, res, next){
 // Profile
 router.get('/profile', function(req, res, next){
     res.send('Profile');
-});
-
-// Validate
-router.get('/validate', function(req, res, next){
-    res.send('Validate');
 });
 
 module.exports = router;
