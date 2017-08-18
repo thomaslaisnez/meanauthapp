190d92f7b90ff6a196221ee15b42120abf8ec4a8
Thomas <thomaslaisnez@live.be>
2017-08-18 19:00:41 +0200

    added e2e tests

--- angular-src/e2e/app.e2e-spec.ts
+++ angular-src/e2e/app.e2e-spec.ts
@@ -1,14 +1,47 @@
-import { AngularSrcPage } from './app.po';
+//import { AngularSrcPage } from './app.po';
+import { element, by, browser } from 'protractor';
 
-describe('angular-src App', function() {
-  let page: AngularSrcPage;
+describe('MEAN Auth App test', function() {
 
-  beforeEach(() => {
-    page = new AngularSrcPage();
+  it('should display home title', () => {
+    browser.get('/');
+    let title = element(by.tagName('h1')).getText();
+    expect(title).toEqual('MEAN Auth App');
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  it('should be on login', () => {
+    browser.get('/login');
+    var currentUrl = browser.getCurrentUrl();
+    expect(currentUrl).toMatch('/login');
+  });
+
+  it('should be on home because we are not logged in', () => {
+    browser.get('/profile');
+    var currentUrl = browser.getCurrentUrl();
+    expect(currentUrl).toMatch('/');
+  });
+
+  it('should go to login after registration', () => {
+    browser.get('/register');
+    element(by.id('usernameField')).sendKeys('testUser');
+    element(by.id('nameField')).sendKeys('testName');
+    element(by.id('emailField')).sendKeys('test@test.com');
+    element(by.id('passwordField')).sendKeys('test');
+    let submitButton = element(by.id('submitButton'));
+    submitButton.click();
+    //expect(browser.driver.getCurrentUrl()).toBe('/dashboard');
+    var currentUrl = browser.getCurrentUrl();
+    expect(currentUrl).toMatch('/login');
+  });
+
+  it('should login', () => {
+    browser.get('/login');
+    element(by.id('usernameField')).sendKeys('test');
+    element(by.id('passwordField')).sendKeys('test');
+    let submitButton = element(by.id('loginButton'));
+    submitButton.click();
+    //expect(browser.driver.getCurrentUrl()).toBe('/dashboard');
+    var currentUrl = browser.getCurrentUrl();
+    expect(currentUrl).toMatch('/dashboard');
   });
 });
