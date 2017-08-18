89ade114fc6d0c2010ff26dade3e28555c2a1303
Thomas <thomaslaisnez@live.be>
2017-08-18 19:34:41 +0200

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    //database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://testuser:test@ds145263.mlab.com:45263/meanauthapp',
     secret: 'yoursecret'
 }
